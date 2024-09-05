3a94147eeb2d7e0c19f44bf7e6be3f1c98f0f7ce
Rebecca <rebecca@croysdale.net>
2024-09-05 11:12:02 -0700

    Adds initial spring boot backend

---
+++ back-end/src/main/java/app/cluttermap/ClutterMapApplication.java
@@ -0,0 +1,14 @@
+package app.cluttermap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClutterMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClutterMapApplication.class, args);
+	}
+
+}
+
