208db35ce83ce27654421675cbd17c47587e583e
Rebecca <rebecca@croysdale.net>
2024-09-24 13:56:34 -0700

    Connect RoomsController to RoomRepository and enables a few crud API queries

--- back-end/src/main/java/app/cluttermap/controller/RoomsController.java
+++ back-end/src/main/java/app/cluttermap/controller/RoomsController.java
@@ -1,26 +1,58 @@
 package app.cluttermap.controller;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import app.cluttermap.model.Room;
-
-import java.util.Arrays;
-import java.util.List;
+import app.cluttermap.repository.RoomsRepository;
 
 @RestController
+@RequestMapping("/rooms")
 @CrossOrigin(origins = "http://localhost:3000")
 public class RoomsController {
-    @GetMapping("/rooms")
-    public List<Room> getRooms() {
-        System.out.println("GET /rooms called");
+    @Autowired
+    private final RoomsRepository roomsRepository;
 
-        return Arrays.asList(
-                new Room(1001L, "Kitchen", "The kitchen"),
-                new Room(1002L, "Living Room", "The room with the tv"),
-                new Room(1003L, "Master Bedroom", "The main bedroom"));
+    public RoomsController(RoomsRepository roomsRepository) {
+        this.roomsRepository = roomsRepository;
+    }
+
+    @GetMapping()
+    public Iterable<Room> getRooms() {
+        return this.roomsRepository.findAll();
+    }
+
+    @PostMapping()
+    public ResponseEntity<Room> addOneRoom(@RequestBody Room room) {
+        if (room.getName() == null || room.getName().isEmpty()){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        Room savedRoom = this.roomsRepository.save(room);
+        return new ResponseEntity<>(savedRoom, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Room> getOneRoom(@PathVariable("id") Long id) {
+        Optional<Room> roomData = roomsRepository.findById(id);
+
+        if (roomData.isPresent()) {
+            return new ResponseEntity<>(roomData.get(), HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 }
 
