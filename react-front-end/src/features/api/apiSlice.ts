4c361c08432b1942119cdf56b76a13e780c17db1
Rebecca <rebecca@croysdale.net>
2024-09-25 11:20:31 -0700

    Adds 'Add a New Room' page and redux query

--- react-front-end/src/features/api/apiSlice.ts
+++ react-front-end/src/features/api/apiSlice.ts
@@ -1,6 +1,5 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
-import { Room } from '@/types/types'
 import { API_BASE_URL } from '@/utils/constants'
 import { Room, NewRoom } from '../rooms/roomsSlice'
 
@@ -12,10 +11,19 @@ export const apiSlice = createApi({
     endpoints: builder => ({
         getRooms: builder.query<Room[], void>({
             query: () => '/rooms'
+        }),
+
+        addNewRoom: builder.mutation<Room, NewRoom> ({
+            query: initialRoom => ({
+                url: '/rooms',
+                method: 'POST',
+                body: initialRoom
+            })
         })
     })
 })
 
 export const {
-    useGetRoomsQuery
+    useGetRoomsQuery,
+    useAddNewRoomMutation
 } = apiSlice
--- react-front-end/src/routes/routes.tsx
+++ react-front-end/src/routes/routes.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import RoomsList from '@/features/rooms/RoomsList';
+import { AddRoom } from '@/features/rooms/AddRoom';
 
 const Pages: React.FC = () => {
     return (
@@ -9,6 +10,7 @@ const Pages: React.FC = () => {
                 {/* <Route exact path="/" component={Home} /> */}
                 {/* <Route path="/add-item" component={AddItem} /> */}
                 <Route path="/rooms" Component={RoomsList} /> 
+                <Route path="/rooms/add" Component={AddRoom} /> 
             </Routes>
         </BrowserRouter>
     );
