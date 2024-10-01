e3484d4a21f624f4a0a4b4b779ddd0888d4f1697
Rebecca <rebecca@croysdale.net>
2024-10-01 15:10:20 -0700

    Adds google id to User entity

--- back-end/src/main/java/app/cluttermap/model/User.java
+++ back-end/src/main/java/app/cluttermap/model/User.java
@@ -1,11 +1,14 @@
 package app.cluttermap.model;
 
+import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.util.Date;
 
 @Entity
 @Table(name = "app_user")
@@ -14,10 +17,34 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String googleId;
     private String username;
     private String email;
     private String provider; // 'google'
 
+
+    @CreationTimestamp
+    @Column(updatable = false, name = "created_at")
+    private Date createdAt;
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getGoogleId() {
+        return googleId;
+    }
+    public void setGoogleId(String googleId) {
+        this.googleId = googleId;
+    }
     public String getUsername() {
         return username;
     }
--- back-end/src/main/java/app/cluttermap/repository/UsersRepository.java
+++ back-end/src/main/java/app/cluttermap/repository/UsersRepository.java
@@ -10,4 +10,5 @@ import app.cluttermap.model.User;
 @Repository
 public interface UsersRepository extends CrudRepository<User, Long> {
     Optional <User> findByEmail(String email);
+    Optional <User> findByGoogleId(String googleId);
 }
