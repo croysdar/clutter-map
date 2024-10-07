f4e86d121a8857d09b0262a0746e6d15ad6f4b95
croysdaa <alannacroysdale@gmail.com>
2024-10-07 12:03:35 -0700

    Adds project entity

---
+++ back-end/src/main/java/app/cluttermap/model/Project.java
@@ -0,0 +1,40 @@
+package app.cluttermap.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table (name = "projects")
+
+public class Project {
+    
+    @Id
+    // Postgres generates an ID
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+}
