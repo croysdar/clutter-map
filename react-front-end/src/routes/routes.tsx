2e75bda704675a45240658540e6fe1c70c06191e
croysdaa <alannacroysdale@gmail.com>
2024-10-09 14:15:02 -0700

    Adds routes for Project pages

--- react-front-end/src/routes/routes.tsx
+++ react-front-end/src/routes/routes.tsx
@@ -3,6 +3,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import RoomsList from '@/features/rooms/RoomsList';
 import { AddRoom } from '@/features/rooms/AddRoom';
 import EditRoom from '@/features/rooms/EditRoom';
+import ProjectsList from '@/features/projects/ProjectsList';
+import { AddProject } from '@/features/projects/AddProject';
+import EditProject from '@/features/projects/EditProject';
 
 const Pages: React.FC = () => {
     return (
@@ -12,10 +15,13 @@ const Pages: React.FC = () => {
                 {/* <Route path="/add-item" component={AddItem} /> */}
                 {/* <Route path="/rooms" Component={RoomsList} /> 
                 <Route path="/rooms/add" Component={AddRoom} /> 
-                <Route path="/rooms/:roomID/edit" Component={EditRoom} /> */}
-                <Route path="/projects/:projectID/rooms" Component={RoomsList} /> 
-                <Route path="/projects/:projectID/rooms/add" Component={AddRoom} /> 
-                <Route path="/projects/:projectID/rooms/:roomID/edit" Component={EditRoom} /> 
+                <Route path="/rooms/:roomId/edit" Component={EditRoom} /> */}
+                <Route path="/projects/:projectId/rooms" Component={RoomsList} /> 
+                <Route path="/projects/:projectId/rooms/add" Component={AddRoom} /> 
+                <Route path="/projects/:projectId/rooms/:roomId/edit" Component={EditRoom} /> 
+                <Route path="/projects" Component={ProjectsList} />
+                <Route path="/projects/add" Component={AddProject} /> 
+                <Route path="/projects/:projectId/edit" Component={EditProject} /> 
             </Routes>
         </BrowserRouter>
     );
