f835f5f0172db1083bc11ad50a4ffabda2126f89
croysdaa <alannacroysdale@gmail.com>
2024-10-16 14:34:34 -0700

    Removed Unnecessary Imports

--- back-end/src/main/java/app/cluttermap/controller/ProjectsController.java
+++ back-end/src/main/java/app/cluttermap/controller/ProjectsController.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +18,6 @@ import org.springframework.web.bind.annotation.RestController;
 import app.cluttermap.model.Project;
 import app.cluttermap.model.Room;
 import app.cluttermap.repository.ProjectsRepository;
-import app.cluttermap.repository.RoomsRepository;
 
 @RestController
 @RequestMapping("/projects")
