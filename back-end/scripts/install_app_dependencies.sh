7ca4f822ee98f7883ad42f65e7f2a49374e05218
Rebecca <rebecca@croysdale.net>
2024-10-18 13:25:55 -0700

    Adjusting install dependencies
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging

--- back-end/scripts/install_app_dependencies.sh
+++ back-end/scripts/install_app_dependencies.sh
@@ -8,10 +8,17 @@ if ! java -version 2>&1 | grep -q "21"; then
     sudo yum install -y java-21-amazon-corretto-headless.x86_64 
 fi
 
-sudo yum install -y supervisor
+if ! command -V pip 2>&1; then
+    sudo yum install python3-pip
+fi
 
-# # Make sure Gradle wrapper has execute permission
-# chmod +x ./gradlew
+if ! command -v supervisord 2>&1; then
+    sudo pip install supervisor
+fi
+
+if ! command -v /usr/local/bin/supervisord &> /dev/null; then
+    sudo pip3 install supervisor
+fi
+
 # Ensure the JAR is executable (if needed)
-# chmod +x build/libs/clutter-map-0.0.1-SNAPSHOT.jar
 chmod +x clutter-map-0.0.1-SNAPSHOT.jar
--- back-end/scripts/start_server.sh
+++ back-end/scripts/start_server.sh
@@ -3,9 +3,10 @@
 # # Navigate to the deployment directory
 cd /home/ec2-user/deployment
 
-# Ensure the JAR is executable (if needed)
-# chmod +x build/libs/clutter-map-0.0.1-SNAPSHOT.jar
-# chmod +x clutter-map-0.0.1-SNAPSHOT.jar
+export PATH=$PATH:/usr/local/bin
+
+# End any existing supervisor processes
+sudo pkill supervisord
 
 # Start the Spring Boot application using supervisor
 supervisord -c supervisord.conf
