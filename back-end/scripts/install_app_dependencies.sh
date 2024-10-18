3b58edaf127aa0808113b189c3c6724a55528375
Rebecca <rebecca@croysdale.net>
2024-10-17 18:35:31 -0700

    debugging
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging
    
    debugging

--- back-end/scripts/install_app_dependencies.sh
+++ back-end/scripts/install_app_dependencies.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 # Navigate to the project directory
-cd /home/ec2-user/cluttermap-app/back-end
+cd /home/ec2-user/deployment
 
 # Install Java 21 if not installed
 if ! java -version 2>&1 | grep -q "21"; then
     sudo yum install -y java-21-amazon-corretto-headless.x86_64 
 fi
 
-# Make sure Gradle wrapper has execute permission
-chmod +x ./gradlew
+sudo yum install -y supervisor
+
+# # Make sure Gradle wrapper has execute permission
+# chmod +x ./gradlew
+# Ensure the JAR is executable (if needed)
+# chmod +x build/libs/clutter-map-0.0.1-SNAPSHOT.jar
+chmod +x clutter-map-0.0.1-SNAPSHOT.jar
--- back-end/scripts/start_server.sh
+++ back-end/scripts/start_server.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# Navigate to the Java Spring app directory
-cd /home/ec2-user/clutter-map/back-end
+# # Navigate to the deployment directory
+cd /home/ec2-user/deployment
 
 # Ensure the JAR is executable (if needed)
-chmod +x build/libs/clutter-map-0.0.1-SNAPSHOT.jar
+# chmod +x build/libs/clutter-map-0.0.1-SNAPSHOT.jar
+# chmod +x clutter-map-0.0.1-SNAPSHOT.jar
 
 # Start the Spring Boot application using supervisor
 supervisord -c supervisord.conf
