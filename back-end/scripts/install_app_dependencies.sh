48da4e2c44a02314216c02b87fc28ff77c2ebba0
Rebecca <rebecca@croysdale.net>
2024-10-18 13:35:02 -0700

    debugging

--- back-end/scripts/install_app_dependencies.sh
+++ back-end/scripts/install_app_dependencies.sh
@@ -16,5 +16,9 @@ if ! command -v supervisord 2>&1; then
     sudo pip install supervisor
 fi
 
+if ! command -v /usr/local/bin/supervisord &> /dev/null; then
+    sudo pip3 install supervisor
+fi
+
 # Ensure the JAR is executable (if needed)
 chmod +x clutter-map-0.0.1-SNAPSHOT.jar
