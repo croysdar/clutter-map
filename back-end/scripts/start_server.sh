106630b7267c281988e4c2a78fbceada22e912f8
Rebecca <rebecca@croysdale.net>
2024-10-18 13:30:09 -0700

    debugging

--- back-end/scripts/start_server.sh
+++ back-end/scripts/start_server.sh
@@ -3,9 +3,5 @@
 # # Navigate to the deployment directory
 cd /home/ec2-user/deployment
 
-# Ensure the JAR is executable (if needed)
-# chmod +x build/libs/clutter-map-0.0.1-SNAPSHOT.jar
-# chmod +x clutter-map-0.0.1-SNAPSHOT.jar
-
 # Start the Spring Boot application using supervisor
 supervisord -c supervisord.conf
