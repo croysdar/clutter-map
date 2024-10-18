4dc281d17bbc08f0c09a2b4f19d99c109b4a3ac9
Rebecca <rebecca@croysdale.net>
2024-10-18 13:35:29 -0700

    debugging

--- back-end/scripts/start_server.sh
+++ back-end/scripts/start_server.sh
@@ -3,5 +3,7 @@
 # # Navigate to the deployment directory
 cd /home/ec2-user/deployment
 
+export PATH=$PATH:/usr/local/bin
+
 # Start the Spring Boot application using supervisor
 supervisord -c supervisord.conf
