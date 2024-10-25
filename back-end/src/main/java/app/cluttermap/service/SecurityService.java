9c9ebf4a07e19e7208a0cb411461fa03425692f3
Rebecca <rebecca@croysdale.net>
2024-10-25 15:55:42 -0700

    Adds isResourceOwner and getUserFromAuth

---
+++ back-end/src/main/java/app/cluttermap/service/SecurityService.java
@@ -0,0 +1,87 @@
+package app.cluttermap.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.stereotype.Service;
+
+import app.cluttermap.model.OrgUnit;
+import app.cluttermap.model.Project;
+import app.cluttermap.model.Room;
+import app.cluttermap.model.User;
+import app.cluttermap.repository.OrgUnitsRepository;
+import app.cluttermap.repository.ProjectsRepository;
+import app.cluttermap.repository.RoomsRepository;
+import app.cluttermap.repository.UsersRepository;
+
+@Service("securityService")
+public class SecurityService {
+    @Autowired
+    private final ProjectsRepository projectsRepository;
+
+    @Autowired
+    private final RoomsRepository roomsRepository;
+
+    @Autowired
+    private final OrgUnitsRepository orgUnitsRepository;
+
+    @Autowired
+    private final UsersRepository usersRepository;
+
+    public SecurityService(UsersRepository usersRepository, ProjectsRepository projectsRepository, RoomsRepository roomsRepository, OrgUnitsRepository orgUnitsRepository) {
+        this.usersRepository = usersRepository;
+        this.projectsRepository = projectsRepository;
+        this.roomsRepository = roomsRepository;
+        this.orgUnitsRepository = orgUnitsRepository;
+    }
+
+    public Long getUserIdFromAuthentication(Authentication authentication) {
+        return getUserFromAuthentication(authentication).getId();
+    }
+
+    public User getUserFromAuthentication(Authentication authentication) {
+        if (authentication == null || !(authentication instanceof JwtAuthenticationToken)) {
+            throw new IllegalStateException("Authentication does not contain a JWT token");
+        }
+
+        Jwt jwt = ((JwtAuthenticationToken) authentication).getToken();
+        Long user_id = Long.valueOf(jwt.getSubject());
+
+        return usersRepository.findById(user_id).orElseThrow(() -> new IllegalStateException("User does not exist"));
+    }
+
+    public boolean isResourceOwner(Authentication authentication, Long resourceId, String resourceType) {
+        Long currentUserId = getUserIdFromAuthentication(authentication);
+
+        switch (resourceType) {
+            case "project" :
+                Optional <Project> projectData = projectsRepository.findById(resourceId);
+                if (!projectData.isPresent()) {
+                    throw new IllegalStateException("Project does not exist");
+                }
+
+                return projectData.get().getOwner().getId().equals(currentUserId);
+
+            case "room" :
+                Optional <Room> roomData = roomsRepository.findById(resourceId);
+                if (!roomData.isPresent()) {
+                    throw new IllegalStateException("Room does not exist");
+                }
+
+                return roomData.get().getProject().getOwner().getId().equals(currentUserId);
+
+            case "org-unit" :
+                Optional <OrgUnit> orgUnitData = orgUnitsRepository.findById(resourceId);
+                if (!orgUnitData.isPresent()) {
+                    throw new IllegalStateException("Organization Unit does not exist");
+                }
+
+                return orgUnitData.get().getRoom().getProject().getOwner().getId().equals(currentUserId);
+        }
+
+        return false;
+    }
+}
