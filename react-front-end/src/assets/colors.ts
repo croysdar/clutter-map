265fcbba7d9b20f9060f33e7014995693cf67faf
Rebecca <rebecca@croysdale.net>
2024-10-31 15:21:36 -0700

    Adds a colors file

---
+++ react-front-end/src/assets/colors.ts
@@ -0,0 +1,18 @@
+export const appColors = {
+    primary: '#1976d2',
+    primaryLight: '#63a4ff',
+    primaryDark: '#004ba0',
+
+    secondary: '#ff4081',
+    secondaryLight: '#ff79b0',
+    secondaryDark: '#c60055',
+
+    backgroundDefault: '#f5f5f5',
+    backgroundPaper: '#ffffff',
+
+    textPrimary: '#000000',
+    textSecondary: '#90A4AE',
+
+    textWhite: '#ffffff'
+
+};
