b5d0df1a3932c10ab32e0b07fdf82943cedd5e2b
Rebecca <rebecca@croysdale.net>
2024-11-04 11:25:24 -0800

    Fixes missing imports

--- back-end/src/main/java/app/cluttermap/model/OrgUnit.java
+++ back-end/src/main/java/app/cluttermap/model/OrgUnit.java
@@ -1,7 +1,12 @@
 package app.cluttermap.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -10,6 +15,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
