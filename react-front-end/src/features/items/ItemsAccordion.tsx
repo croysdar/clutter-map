3da69d93a5c50f593c33c68920771f2de8bdb92c
Rebecca <rebecca@croysdale.net>
2024-11-15 11:45:29 -0800

    Only show item quantity when more than 1

--- react-front-end/src/features/items/ItemsAccordion.tsx
+++ react-front-end/src/features/items/ItemsAccordion.tsx
@@ -71,15 +71,18 @@ const ItemsAccordion: React.FC<ItemsAccordionProps> = ({ orgUnitId }) => {
                     </AccordionSummary>
                     <AccordionDetails>
                         {items.map((item) => (
-                            <Card key={`item-card-${item.id}`} sx={{mt: 1}}>
+                            <Card key={`item-card-${item.id}`} sx={{ mt: 1 }}>
                                 <CardHeader
                                     title={<Typography variant='h6'> {item.name}</Typography>}
                                     action={<ItemMenu item={item} />}
-                                    sx={{py: 1}}
+                                    sx={{ py: 1 }}
                                 />
-                                <CardContent sx={{py: 1}}>
+                                <CardContent sx={{ py: 1 }}>
                                     <Typography variant="body2" gutterBottom align="left">{item.description}</Typography>
-                                    <Typography variant="body2" gutterBottom align="left">Quantity: {item.quantity}</Typography>
+                                    {
+                                        item.quantity > 1 &&
+                                        <Typography variant="body2" gutterBottom align="left">Quantity: {item.quantity}</Typography>
+                                    }
                                 </CardContent>
                                 <CardActions>
                                     <RenderTags tags={item.tags} />
