89b38109c7094a39ae571e93c8d776fb00eb9366
Rebecca <rebecca@croysdale.net>
2024-11-24 14:38:47 -0800

    Adds optional subtitle to EditPageWrapper

--- react-front-end/src/components/pageWrappers/EditPageWrapper.tsx
+++ react-front-end/src/components/pageWrappers/EditPageWrapper.tsx
@@ -3,18 +3,24 @@ import React from 'react';
 
 export type WrapperProps = {
     title: string
+    subtitle?: string
     children: React.ReactNode
 }
 
-export const EditCardWrapper: React.FC<WrapperProps> = ({ title, children }) => {
+export const EditCardWrapper: React.FC<WrapperProps> = ({ title, subtitle, children }) => {
 
     return (
-
         <Card sx={{ width: '100%', padding: 4, boxShadow: 3 }}>
             <CardHeader
                 title={
-                    <Typography variant="h4" component="h2" gutterBottom align="center">
+                    <Typography variant="h4" gutterBottom align="center">
                         {title}
+                    </Typography>
+                }
+                subheader={
+                    subtitle &&
+                    <Typography variant="subtitle1" gutterBottom align="center">
+                        {subtitle}
                     </Typography>
                 }
             />
