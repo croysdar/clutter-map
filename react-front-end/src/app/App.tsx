47fc22b76701d1249a2048cc750b5ce3b0084772
Rebecca <rebecca@croysdale.net>
2025-03-04 13:14:50 -0800

    Redirects from mutation pages when offline

--- react-front-end/src/app/App.tsx
+++ react-front-end/src/app/App.tsx
@@ -35,6 +35,7 @@ import { useAppDispatch, useAppSelector } from '@/hooks/useAppHooks';
 /* ------------- Constants ------------- */
 import { ROUTES } from '@/utils/constants';
 
+import RequiresOnline from '@/features/offline/RequiresOnline';
 import '../assets/styles/App.css';
 
 const ProtectedRoute = ({ children }: { children: ReactElement }) => {
@@ -115,25 +116,78 @@ function App() {
                                     <Routes>
                                         <Route path={ROUTES.projects} Component={ProjectsList} />
 
-                                        <Route path={ROUTES.projectAdd} Component={AddProject} />
+                                        {/* Project Routes */}
+                                        <Route path={ROUTES.projectAdd} element={
+                                            <RequiresOnline redirectUrl={ROUTES.projects}>
+                                                <AddProject />
+                                            </RequiresOnline>
+                                        } />
                                         <Route path={ROUTES.projectDetails(":projectId")} Component={ProjectDetails} />
-                                        <Route path={ROUTES.projectEdit(":projectId")} Component={EditProject} />
+                                        <Route path={ROUTES.projectEdit(":projectId")} element={
+                                            <RequiresOnline redirectUrl={ROUTES.projectDetails}>
+                                                <EditProject />
+                                            </RequiresOnline>
+                                        } />
 
-                                        <Route path={ROUTES.roomAdd(":projectId")} Component={AddRoom} />
+                                        {/* Room Routes */}
+                                        <Route path={ROUTES.roomAdd(":projectId")} element={
+                                            <RequiresOnline redirectUrl={ROUTES.projectDetails}>
+                                                <AddRoom />
+                                            </RequiresOnline>
+                                        } />
                                         <Route path={ROUTES.roomDetails(":projectId", ":roomId")} Component={RoomDetails} />
-                                        <Route path={ROUTES.roomEdit(":projectId", ":roomId")} Component={EditRoom} />
-                                        <Route path={ROUTES.roomRemoveOrgUnits(":projectId", ":roomId")} Component={RemoveRoomOrgUnits} />
-                                        <Route path={ROUTES.roomAssignOrgUnits(":projectId", ":roomId")} Component={AssignOrgUnitsToRoom} />
+                                        <Route path={ROUTES.roomEdit(":projectId", ":roomId")} element={
+                                            <RequiresOnline redirectUrl={ROUTES.roomDetails}>
+                                                <EditRoom />
+                                            </RequiresOnline>
+                                        } />
+                                        <Route path={ROUTES.roomRemoveOrgUnits(":projectId", ":roomId")} element={
+                                            <RequiresOnline redirectUrl={ROUTES.roomDetails}>
+                                                <RemoveRoomOrgUnits />
+                                            </RequiresOnline>
+                                        } />
 
-                                        <Route path={ROUTES.orgUnitAdd(":projectId", ":roomId")} Component={AddOrgUnit} />
+                                        <Route path={ROUTES.roomAssignOrgUnits(":projectId", ":roomId")} element={
+                                            <RequiresOnline redirectUrl={ROUTES.roomDetails}>
+                                                <AssignOrgUnitsToRoom />
+                                            </RequiresOnline>
+                                        } />
+
+                                        {/* Org Unit Routes */}
+                                        <Route path={ROUTES.orgUnitAdd(":projectId", ":roomId")} element={
+                                            <RequiresOnline redirectUrl={ROUTES.roomDetails}>
+                                                <AddOrgUnit />
+                                            </RequiresOnline>
+                                        } />
                                         <Route path={ROUTES.orgUnitDetails(":projectId", ":roomId", ":orgUnitId")} Component={OrgUnitDetails} />
-                                        <Route path={ROUTES.orgUnitEdit(":projectId", ":roomId", ":orgUnitId")} Component={EditOrgUnit} />
-                                        <Route path={ROUTES.orgUnitRemoveItems(":projectId", ":roomId", ":orgUnitId")} Component={RemoveOrgUnitItems} />
-                                        <Route path={ROUTES.orgUnitAssignItems(":projectId", ":roomId", ":orgUnitId")} Component={AssignItemsToOrgUnit} />
+                                        <Route path={ROUTES.orgUnitEdit(":projectId", ":roomId", ":orgUnitId")} element={
+                                            <RequiresOnline redirectUrl={ROUTES.orgUnitDetails}>
+                                                <EditOrgUnit />
+                                            </RequiresOnline>
+                                        } />
+                                        <Route path={ROUTES.orgUnitRemoveItems(":projectId", ":roomId", ":orgUnitId")} element={
+                                            <RequiresOnline redirectUrl={ROUTES.orgUnitDetails}>
+                                                <RemoveOrgUnitItems />
+                                            </RequiresOnline>
+                                        } />
+                                        <Route path={ROUTES.orgUnitAssignItems(":projectId", ":roomId", ":orgUnitId")} element={
+                                            <RequiresOnline redirectUrl={ROUTES.orgUnitDetails}>
+                                                <AssignItemsToOrgUnit />
+                                            </RequiresOnline>
+                                        } />
 
-                                        <Route path={ROUTES.itemAdd(":projectId", ":roomId", ":orgUnitId")} Component={AddItem} />
+                                        {/* Item Routes */}
+                                        <Route path={ROUTES.itemAdd(":projectId", ":roomId", ":orgUnitId")} element={
+                                            <RequiresOnline redirectUrl={ROUTES.orgUnitDetails}>
+                                                <AddItem />
+                                            </RequiresOnline>
+                                        } />
                                         <Route path={ROUTES.itemDetails(":projectId", ":roomId", ":orgUnitId", ":itemId")} Component={ItemDetails} />
-                                        <Route path={ROUTES.itemEdit(":projectId", ":roomId", ":orgUnitId", ":itemId")} Component={EditItem} />
+                                        <Route path={ROUTES.itemEdit(":projectId", ":roomId", ":orgUnitId", ":itemId")} element={
+                                            <RequiresOnline redirectUrl={ROUTES.itemDetails}>
+                                                <EditItem />
+                                            </RequiresOnline>
+                                        } />
                                     </Routes>
                                 </ProtectedRoute>
                             }
