3d2e01a8fc44012b648da059505a76cb0a3a5aa8
Rebecca <rebecca@croysdale.net>
2025-05-08 14:24:14 -0700

    Fixes aria-hidden related warning

--- react-front-end/src/features/search/SearchManager.tsx
+++ react-front-end/src/features/search/SearchManager.tsx
@@ -5,9 +5,17 @@ import { SearchModal } from "./SearchModal";
 export const SearchManager: React.FC = () => {
     const [searchOpen, setSearchOpen] = useState(false);
 
+    const handleOpenSearch = () => {
+        // Blur any focused element (like the FAB) before opening the modal
+        if (document.activeElement instanceof HTMLElement) {
+            document.activeElement.blur();
+        }
+        setSearchOpen(true);
+    };
+
     return (
         <>
-            <SearchToggle onOpenSearch={() => setSearchOpen(true)} />
+            <SearchToggle onOpenSearch={() => handleOpenSearch()} />
             <SearchModal open={searchOpen} onClose={() => setSearchOpen(false)} />
         </>
     );
