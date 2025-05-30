3a05d29e52652a6528fcc6ed1a32f6c5ca147b92
Rebecca <rebecca@croysdale.net>
2025-05-29 17:00:39 -0700

    Swaps to VITE environment variables for backend API URL

--- react-front-end/src/utils/constants.ts
+++ react-front-end/src/utils/constants.ts
@@ -1,4 +1,4 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const AUTH_CLIENT_ID = "890412284296-t0hakf6vsvgdubtoulcnp8cmk4i6hon1.apps.googleusercontent.com"
 
